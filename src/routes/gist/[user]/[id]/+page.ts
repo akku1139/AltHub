e866c8c9daf1b82e4441a9fe7bf6197a03e6d0ad
-akku- <151517253+akku1139@users.noreply.github.com>
2024-01-28 22:00:48 +0900

    wip

--- src/routes/gist/[user]/[id]/+page.ts
+++ src/routes/gist/[user]/[id]/+page.ts
@@ -3,8 +3,9 @@ import { error } from '@sveltejs/kit';
 import type { PageLoad } from "./$types";
 
 export async function load({ params }): PageLoad {
-	const doc = htmlparser2.parseDocument((await fetch(`https://gist.github.com/${params.user}/${params.id}`)).text())
+	const res = (await fetch(`https://gist.github.com/${params.user}/${params.id}`)).text();
+	const doc = htmlparser2.parseDocument(res);
 	return {
-		title: ('div[@itemprop="about"]')
+		title: /<div itemprop="about">.*?<\/div>/.exec(res)[0].trim()
 	};
 }
