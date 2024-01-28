39bfd3a18aeba15a6e424301da81fb8da5720e37
-akku- <151517253+akku1139@users.noreply.github.com>
2024-01-28 19:25:35 +0900

    fix await

--- src/routes/gist/[user]/[id]/+page.ts
+++ src/routes/gist/[user]/[id]/+page.ts
@@ -3,11 +3,11 @@ import { error } from '@sveltejs/kit';
 import { PageLoad } from "./$types";
 
 export function load({ params }): PageLoad {
-	const res = await fetch(`https://gist.github.com/${params.user}/${params.id}`)
-	if(res.status !== 200){
-		error(404, 'Not found');
-		return;
-	}
-	const doc = libxmljs.parseXml(res.text());
-	
+	const res = fetch(`https://gist.github.com/${params.user}/${params.id}`).then((res) => {
+		if(res.status !== 200){
+			error(404, 'Not found');
+			return;
+		}
+		const doc = libxmljs.parseXml(res.text());
+	})
 }
