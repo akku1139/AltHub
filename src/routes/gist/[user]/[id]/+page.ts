508b0368aec3f5df762c6f45320f7d36736b2a77
-akku- <151517253+akku1139@users.noreply.github.com>
2024-01-28 20:22:08 +0900

    jsdom

--- src/routes/gist/[user]/[id]/+page.ts
+++ src/routes/gist/[user]/[id]/+page.ts
@@ -1,16 +1,11 @@
-import libxmljs from "libxmljs";
+import { JSDOM } from 'jsdom';
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from "./$types";
 
-export function load({ params }): PageLoad {
-	fetch(`https://gist.github.com/${params.user}/${params.id}`).then((res) => {
-		if(res.status !== 200){
-			error(404, 'Not found');
-			return;
-		}
-		res.text().then((_text) => {
-			const doc = libxmljs.parseXml(_text);
-			doc.find('//div[@itemprop="about"]')[0].text();
-		});
-	});
+export async function load({ params }): PageLoad {
+	const dom = JSDOM.fromURL(`https://gist.github.com/${params.user}/${params.id}`);
+	const document:Document = dom.window.document;
+	return {
+		title: document.querySelector('div[@itemprop="about"]')?.innerHTML
+	}
 }
