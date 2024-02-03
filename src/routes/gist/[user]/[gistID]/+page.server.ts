3f37b2b07718ee3e64bc109eef1d0f8ffd8502ca
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-03 23:18:34 +0900

    a

--- src/routes/gist/[user]/[gistID]/+page.server.ts
+++ src/routes/gist/[user]/[gistID]/+page.server.ts
@@ -1,3 +1,4 @@
+import { parse } from 'node-html-parser';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from "./$types";
 
@@ -32,7 +33,7 @@ type Gist = {
 
 export async function load({ params }): PageServerLoad {
 	const res = await (await fetch(`https://gist.github.com/${params.user}/${params.gistID}`)).text();
-	//const doc = htmlparser2.parseDocument(res);
+	const doc = await parse(res);
 	return {
 		title: new RegExp(`<a href="/${params.user}/${params.gistID}">(.*?)</a>`).exec(res)[1],
 		description: /<div itemprop="about">(.*?)<\/div>/s.exec(res)[1].trim(),
