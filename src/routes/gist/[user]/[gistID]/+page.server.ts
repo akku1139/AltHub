5bcd6dce65e27e3a19075631843182d8c24f7b85
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-04 16:55:50 +0900

    childrenが実装されてない?

--- src/routes/gist/[user]/[gistID]/+page.server.ts
+++ src/routes/gist/[user]/[gistID]/+page.server.ts
@@ -6,13 +6,13 @@ export async function load({ params }): Gist {
   const res = await fetch(`https://gist.github.com/${params.user}/${params.gistID}`);
   if(!res.ok) {
     return error(404, {
-			message: 'Not found'
-		});
+      message: 'Not found',
+    });
   }
-  const document = await parse(await res.text());
+  const doc = await parse(await res.text());
   return {
     title: new RegExp(`<a href="/${params.user}/${params.gistID}">(.*?)</a>`).exec(res)[1],
     description: /<div itemprop="about">(.*?)<\/div>/s.exec(res)[1].trim(),
-    forks: document.getElementById("gist-fork-button").children[0].children[2].innerText,
+    forks: doc.querySelector("button#gist-fork-button > * :nth-child(3)").innerText,
   };
 }
