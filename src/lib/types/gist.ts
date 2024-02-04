26e83c3e645a7b6dadfdc9cc52aabb5b69326337
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-04 16:14:29 +0900

    type

---
+++ src/lib/types/gist.ts
@@ -0,0 +1,25 @@
+import { User } from "./user.ts";
+
+export type Gist = {
+  title: string;
+  description: string;
+  user: User;
+  gistID: string;
+  stars: number;
+  forks: number;
+  files: {
+    filename: string;
+    content:  {
+      text?: string; // if basic file
+      url?: string; // if image like file
+    };
+  };
+  revisions: {
+
+  };
+  comments: {
+    user: User;
+    date: Date;
+    contentHistory: string[];
+  };
+};
--- src/routes/gist/[user]/[gistID]/+page.server.ts
+++ src/routes/gist/[user]/[gistID]/+page.server.ts
@@ -2,35 +2,6 @@ import { parse } from 'node-html-parser';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from "./$types";
 
-type User = {
-  id: string;
-  iconURL: string;
-};
-
-type Gist = {
-  title: string;
-  description: string;
-  user: string; // TODO: define another type
-  gistID: string;
-  stars: number;
-  forks: number;
-  files: {
-    filename: string;
-    content:  {
-      text?: string; // if basic file
-      url?: string; // if image like file
-    };
-  };
-  revisions: {
-
-  };
-  comments: {
-    user: User;
-    date: Date;
-    contentHistory: string[];
-  };
-};
-
 export async function load({ params }): PageServerLoad {
   const res = await (await fetch(`https://gist.github.com/${params.user}/${params.gistID}`)).text();
   const doc = await parse(res);
