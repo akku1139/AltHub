cd4be17e1349a37727bd1a847a75ef2fd349aef0
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-04 16:40:51 +0900

    処理

--- src/routes/gist/[user]/[gistID]/+page.server.ts
+++ src/routes/gist/[user]/[gistID]/+page.server.ts
@@ -3,8 +3,13 @@ import { error } from '@sveltejs/kit';
 import type { Gist } from "$lib/types/gist.ts";
 
 export async function load({ params }): Gist {
-  const res = await (await fetch(`https://gist.github.com/${params.user}/${params.gistID}`)).text();
-  const document = await parse(res);
+  const res = await fetch(`https://gist.github.com/${params.user}/${params.gistID}`);
+  if(!res.ok) {
+    return error(404, {
+			message: 'Not found'
+		});
+  }
+  const document = await parse(await res.text());
   return {
     title: new RegExp(`<a href="/${params.user}/${params.gistID}">(.*?)</a>`).exec(res)[1],
     description: /<div itemprop="about">(.*?)<\/div>/s.exec(res)[1].trim(),
