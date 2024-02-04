3900284f9aa7fd9ce647e1d8371c585de64bb2fe
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-04 17:11:21 +0900

    スタックトレース(危険)

---
+++ src/hooks.server.ts
@@ -0,0 +1,7 @@
+import type { HandleServerError } from "@sveltejs/kit";
+
+export async function handleError({ error, event, status, message }): HandleServerError {
+  return {
+    message: error.stack,
+  };
+}
