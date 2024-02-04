82ec60e95844e42b3c7472fae48e1debc2ead5d1
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-04 16:36:39 +0900

    a

--- src/routes/gist/[user]/[gistID]/+page.server.ts
+++ src/routes/gist/[user]/[gistID]/+page.server.ts
@@ -4,7 +4,7 @@ import type { Gist } from "$lib/types/gist.ts";
 
 export async function load({ params }): Gist {
   const res = await (await fetch(`https://gist.github.com/${params.user}/${params.gistID}`)).text();
-  //const document = await parse(res);
+  const document = await parse(res);
   return {
     title: new RegExp(`<a href="/${params.user}/${params.gistID}">(.*?)</a>`).exec(res)[1],
     description: /<div itemprop="about">(.*?)<\/div>/s.exec(res)[1].trim(),
