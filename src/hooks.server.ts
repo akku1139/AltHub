0a471204235adc841e078155e46e176cf11779f3
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-04 17:17:23 +0900

    a

--- src/hooks.server.ts
+++
@@ -1,7 +0,0 @@
-import type { HandleServerError } from "@sveltejs/kit";
-
-export async function handleError({ error, event, status, message }): HandleServerError {
-  return {
-    message: error,
-  };
-}
