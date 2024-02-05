b147c8a91be9e3f6aca608e90e66d88683950887
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-05 19:34:36 +0900

    a

--- src/routes/gist/[user]/[gistID]/+page.server.ts
+++ src/routes/gist/[user]/[gistID]/+page.server.ts
@@ -12,10 +12,16 @@ export async function load({ params }): Promise<Gist> {
   const text = await res.text();
   const doc = await parse(text).querySelector("div.application-main");
   const head = doc.querySelector("div.gisthead");
+
   return {
     title: new RegExp(`<a href="/${params.user}/${params.gistID}">(.*?)</a>`).exec(text)[1],
     description: /<div itemprop="about">(.*?)<\/div>/s.exec(text)[1].trim(),
     stars: Number(head.querySelector("#gist-star-button-count > span > :last-child").innerText),
     forks: Number(head.querySelector("#gist-fork-button > span > :last-child").innerText),
+    files: Array.from(doc.querySelector(".file-box").children).map(file => {
+      return {
+        filename: file.querySelector("strong").innerText,
+      }
+    }),
   };
 }
